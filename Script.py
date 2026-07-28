a9b2e400f8f6c5bdfada5ebfb1702503e71d1c6c
patelboss <83952959+patelboss@users.noreply.github.com>
2026-07-28 09:05:49 +0530

    Comment out deprecated EN class attributes
    Comment out old EN class attribute references for clarity.

--- Script.py
+++ Script.py
@@ -15,10 +15,10 @@ class script:
 
     # The old positional-format strings are replaced by named-kwarg versions in i18n.
     # We expose the EN text as-is so old callers at least get English text.
-    START_TXT        = EN.START
-    STATUS_TXT       = EN.STATUS_TXT
-    LOG_TEXT_G       = EN.LOG_TEXT_G
-    LOG_TEXT_P       = EN.LOG_TEXT_P
+    #START_TXT        = EN.START
+    #STATUS_TXT       = EN.STATUS_TXT
+    #LOG_TEXT_G       = EN.LOG_TEXT_G
+  #  LOG_TEXT_P       = EN.LOG_TEXT_P
    # DELETEMSG        = EN.DELETEMSG
    # MELCOW_ENG       = EN.MELCOW_ENG
     CAPTION          = EN.CAPTION
